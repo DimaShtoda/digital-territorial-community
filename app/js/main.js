64dafc94c1f2e3b3cdc461c97d300ff8375191da
Dima Shtoda <shtoda.info@gmail.com>
2019-12-09 14:49:30 +0200

    added mini-menu section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function () {
 
 
-    
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
 
 })
